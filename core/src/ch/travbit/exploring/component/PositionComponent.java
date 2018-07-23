9a2afc5aef10e39e921596e8300361cfc4390def
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2018-07-23 15:36:20 +0200

    components - color and comments

--- core/src/ch/travbit/exploring/component/PositionComponent.java
+++ core/src/ch/travbit/exploring/component/PositionComponent.java
@@ -3,6 +3,9 @@ package ch.travbit.exploring.component;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * This component represents a position in the world.
+ */
 public class PositionComponent implements Component {
     private Vector2 vector;
 
--- core/src/ch/travbit/exploring/component/VisualComponent.java
+++ core/src/ch/travbit/exploring/component/VisualComponent.java
@@ -1,16 +1,26 @@
 package ch.travbit.exploring.component;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+/**
+ * This component represents a graphical object in the world.
+ */
 public class VisualComponent implements Component {
     private TextureRegion textureRegion;
+    private Color color;
 
-    public VisualComponent(TextureRegion textureRegion) {
+    public VisualComponent(TextureRegion textureRegion, Color color) {
         this.textureRegion = textureRegion;
+        this.color = color;
     }
 
     public TextureRegion getTextureRegion() {
         return textureRegion;
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
